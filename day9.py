8a84c885e5ecd70737f620cbd533891c4cc10424
Abi240y <abiyyudestyan@gmail.com>
2024-06-14 20:20:15 +0700

    Create day9.py
    day 9

---
+++ day9.py
@@ -0,0 +1,10 @@
+# Program percabangan sederhana
+
+angka = int(input("Masukkan sebuah angka: "))
+
+if angka > 0:
+    print("Angka yang dimasukkan adalah positif")
+elif angka < 0:
+    print("Angka yang dimasukkan adalah negatif")
+else:
+    print("Angka yang dimasukkan adalah nol")
